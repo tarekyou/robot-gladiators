e8b5a8dd1e696b8b23bfca2d2401c18af9335e2a
tarekyou <89763835+tarekyou@users.noreply.github.com>
2021-09-21 19:51:24 -0700

    started working

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,1 +1,9 @@
-window.alert("This is an alert! JavaScript is running!")
+var playerName = window.prompt("what is your robot's name?")
+//Note the lack of quotation marks around playerName
+console.log(playerName);
+console.log(10 + 10);
+console.log("Our robot's name is " +  playerName);
+function fight() {
+    window.alert("The fight has begun!")
+}
+//fight();
