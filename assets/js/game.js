018fb1d2c6d309e153cde5e1b448ecb2e7fd3381
tarekyou <89763835+tarekyou@users.noreply.github.com>
2021-09-24 13:04:24 -0700

    added multiple robots to the fight

--- assets/js/game.js
+++ assets/js/game.js
@@ -16,8 +16,9 @@ var enemyHealth = 50;
 var enemyAttack = 12;
 
 var fight = function(enemyName) {
+    while(enemyHealth > 0){
   // Alert players that they are starting the round
-  window.alert("Welcome to Robot Gladiators!");
+ // window.alert("Welcome to Robot Gladiators!");
   var promptFight = window.prompt("Would you like to FIGHT or SKIP this battle? Enter 'FIGHT' or 'SKIP' to choose.");
   // if player choses to fight, then fight
 if (promptFight === "fight" || promptFight === "FIGHT") {
@@ -65,12 +66,15 @@ else {
 } else {
     window.alert("You need to pick a valid option. Try again!");
   }
+}
 };
 
 for(var i = 0 ; i < enemyNames.length; i++) {
+    var pickedEnemyName = enemyNames[i];
+    enemyHealth = 50;
     /*console.log(enemyNames[i]);
     console.log(i);
     console.log(enemyNames[i] + " is at " + i + "index");*/
-    fight(enemyNames[i]);
+    fight(pickedEnemyName);
 }
  // fight();
