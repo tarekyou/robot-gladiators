de2ab87616252485063776f40221c44a44dbd61a
tarekyou <89763835+tarekyou@users.noreply.github.com>
2021-09-24 15:49:50 -0700

    start and end game functions

--- assets/js/game.js
+++ assets/js/game.js
@@ -68,14 +68,15 @@ var fight = function(enemyName) {
 }
 };
 
+var startGame = function() {
+    playerHealth = 100;
+    playerAttack = 10;
+    playerMoney = 10;
 for(var i = 0 ; i < enemyNames.length; i++) {
     if (playerHealth > 0 ){
         window.alert("Welcome to Robot Gladiators! Round" + ( i + 1 ));
-    }
-    else {
-        window.alert ("You have lost your robot in battle! Game over!");
-        break;
-    }
+    
+
     var pickedEnemyName = enemyNames[i];
     enemyHealth = 50;
     /*console.log(enemyNames[i]);
@@ -83,4 +84,28 @@ for(var i = 0 ; i < enemyNames.length; i++) {
     console.log(enemyNames[i] + " is at " + i + "index");*/
     fight(pickedEnemyName);
 }
+else {
+    window.alert ("You have lost your robot in battle! Game over!");
+    break;
+}
+}
+endGame();
+ };
+ var endGame = function () {
+     if (playerHealth > 0){
+         window.alert("Great job, you've survived the game! You now have a score of " + playerMoney + "." );
+     }
+     else {
+     window.alert("You've lost your robot in battle.");
+     }
+     var playAgainConfirm = window.confirm("Would you like to play again?");{
+     if (playAgainConfirm){
+         startGame();
+     }
+     else{
+         window.alert("Thank you for playing Robot Gladiators! Come back soon!");
+     }
+     }
+ };
+startGame();
  // fight();
