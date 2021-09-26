b2acbbbab5a65181b3f6f3e3ec471a56887e59dd
tarekyou <89763835+tarekyou@users.noreply.github.com>
2021-09-25 23:02:10 -0700

    random health and attack values

--- assets/js/game.js
+++ assets/js/game.js
@@ -14,6 +14,10 @@ var enemyNames= ["Roborto", "Amy Android", "Robo Trumble"];
 
 var enemyHealth = 50;
 var enemyAttack = 12;
+var randomNumber = function (min, max) {
+    var value = Math.floor (Math.random() * (max - min + 1) + min);
+    return value;
+};
 
 var fight = function(enemyName) {
     while(playerHealth > 0 && enemyHealth > 0 ){
@@ -28,7 +32,7 @@ var fight = function(enemyName) {
     if (confirmSkip) {
       window.alert(playerName + " has decided to skip this fight. Goodbye!");
       // subtract money from playerMoney for skipping
-      playerMoney = playerMoney - 10;
+      playerMoney = Math.max (0, playerMoney - 10);
       console.log("playerMoney", playerMoney);
       break;
     }
@@ -36,7 +40,8 @@ var fight = function(enemyName) {
   // if player choses to fight, then fight
 
     // remove enemy's health by subtracting the amount set in the playerAttack variable
-    enemyHealth = enemyHealth - playerAttack;
+    var damage = randomNumber(playerAttack - 3, playerAttack);
+    enemyHealth = Math.max(0, enemyHealth - damage);
     console.log(
       playerName + " attacked " + enemyName + ". " + enemyName + " now has " + enemyHealth + " health remaining."
     );
@@ -51,7 +56,8 @@ var fight = function(enemyName) {
     }
   
     // remove player's health by subtracting the amount set in the enemyAttack variable
-    playerHealth = playerHealth - enemyAttack;
+    var damage = randomNumber(enemyAttack - 3, enemyAttack);
+    playerHealth = Math.max (0, playerHealth - damage);
     console.log(
       enemyName + " attacked " + playerName + ". " + playerName + " now has " + playerHealth + " health remaining."
     );
@@ -78,7 +84,7 @@ for(var i = 0 ; i < enemyNames.length; i++) {
     
 
     var pickedEnemyName = enemyNames[i];
-    enemyHealth = 50;
+    enemyHealth = randomNumber(40, 60);
     /*console.log(enemyNames[i]);
     console.log(i);
     console.log(enemyNames[i] + " is at " + i + "index");*/
