bb3f2180ebd470aa4b80b3072ac78da53e276915
tarekyou <89763835+tarekyou@users.noreply.github.com>
2021-09-26 22:53:51 -0700

    high score

--- assets/js/game.js
+++ assets/js/game.js
@@ -98,12 +98,20 @@ else {
 endGame();
  };
  var endGame = function () {
-     if (playerInfo.health > 0){
-         window.alert("Great job, you've survived the game! You now have a score of " + playerInfo.money + "." );
+     window.alert("This game has now eneded. Let's see how you did!");
+     var highScore = localStorage.getItem("highscore");
+     if (highScore === null){
+         highScore = 0;
      }
-     else {
-     window.alert("You've lost your robot in battle.");
+     if (playerInfo.money > highScore){
+         localStorage.setItem("highscore", playerInfo.money);
+         localStorage.setItem("name", playerInfo.name);
+         alert(playerInfo.name + "now has the high score of" + playerInfo.money + "!" );
      }
+     else{
+         alert(playerInfo.name + "did not beat the high score of" + highScore + ". Maybe next time!");
+     }
+     
      var playAgainConfirm = window.confirm("Would you like to play again?");{
      if (playAgainConfirm){
          startGame();
