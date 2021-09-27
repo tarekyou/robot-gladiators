4e6b4349ff24ee5f67fe961ef987d128adc72e9b
tarekyou <89763835+tarekyou@users.noreply.github.com>
2021-09-26 21:37:44 -0700

    shop input

--- assets/js/game.js
+++ assets/js/game.js
@@ -108,21 +108,22 @@ endGame();
  };
  var shop = function() {
      var shopOptionPrompt = window.prompt(
-         "Would you like to REFILL your health, UPGRADE your attack, or LEAVE the store? Please enter one: 'REFILL', 'UPGRADE', or 'LEAVE' to make a choice."
+         "Would you like to REFILL your health, UPGRADE your attack, or LEAVE the store? Please enter one 1 for REFILL, 2 for UPGRADE, or 3 for LEAVE."
      );
+     shopOptionPrompt = parseInt(shopOptionPrompt);
      switch (shopOptionPrompt){
-         case "refill":
-          case "REFILL" :   
+         
+          case 1:   
           playerInfo.refillHealth();
              break;
              
              
-        case  "upgrade":
-        case "UPGRADE":    
+        
+        case 2:    
         playerInfo.upgradeAttack();
             break;
-        case "leave":
-        case "LEAVE":    
+        
+        case 3:    
             window.alert ("Leaving the store.");
             break;
         default:
