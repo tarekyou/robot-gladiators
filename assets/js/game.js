0bc4d8390c77af9e096d27413ef566fe94962bf0
tarekyou <89763835+tarekyou@users.noreply.github.com>
2021-09-26 20:02:56 -0700

    player name fix

--- assets/js/game.js
+++ assets/js/game.js
@@ -127,8 +127,16 @@ endGame();
             break;        
      }
  };
+ var getPlayerName = function(){
+     var name = "";
+     while (name === "" || name === null){
+         name = prompt ("What is your robot's name?");
+     }
+     console.log("Your robot's name is " + name);
+     return name;
+ };
  var playerInfo = {
-    name: window.prompt("What is your robot's name?"),
+    name: getPlayerName(),
     health: 100,
     attack: 10,
     money: 10,
